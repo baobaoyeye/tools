50679c421fdea2626a51d8ba8047833ec522b2b5
baorenyi <baorenyi@baidu.com>
2015-09-28 16:51:56 +0800

    add prepend_file.php

--- prepend_file.php
+++ prepend_file.php
@@ -1,5 +1,31 @@
 <?php
+/**
+ * For use easy,you can add this filepath to your php.ini
+ * option [auto_prepend_file =.../prepend_file.php]
+ * to auto these functions before you call your php script
+ *
+ * @author baorenyi
+ * @date   2015-09-28
+ */
 
+
+
+
+/**
+ * get args informations about now called method
+ * 
+ * Usage: 
+ * $_args = func_get_args();
+ * $log = diy_get_method_args(__METHOD__,$_args);
+ * 
+ * The $log is a string type value, what you want to print for you debug.
+ *
+ * Suggestion:
+ * Don't use this method on your online product code.
+ *
+ * @return {string} args infos
+ * @author baorenyi
+ */
 function diy_get_method_args($method,$args){
     $_f = new ReflectionMethod($method);
     $_result = array();
